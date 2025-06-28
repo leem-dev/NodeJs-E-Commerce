30647beb81b0a9a40cf39f4ea1b2374f4f07c54e
Seleem <alabitseleem@gmail.com>
2025-06-28 23:15:05 +0100

    User registration controller setup with bcrypt for password

--- app/app.js
+++ app/app.js
@@ -7,7 +7,8 @@ import userRoutes from "../routes/usersRoute.js";
 // db Connect
 dbConnect();
 const app = express();
-
+// pass incoming data
+app.use(express.json());
 // routes
 app.use("/", userRoutes);
 
--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,11 +1,36 @@
 import User from "../model/User.js";
+import bcrypt from "bcryptjs";
 
 // @desc      Register user
 // @route     POST /api/v1/users/register
 // @access    Private/Admin
 
 export const registerUserCtrl = async (req, res) => {
-  res.json({
-    msg: "User register controller",
+  const { fullname, email, password } = req.body;
+  // check if users exist
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    // throw
+    res.json({
+      msg: "User already exists",
+    });
+  }
+
+  // hash password
+  // create a salt
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // create the user
+  const user = await User.create({
+    fullname,
+    email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({
+    status: "success",
+    message: "User registered successfully",
+    data: user,
   });
 };
