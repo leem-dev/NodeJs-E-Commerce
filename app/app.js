70dce1e11431452dd74fd092d66e7ca92809fe27
Seleem <alabitseleem@gmail.com>
2025-06-28 12:26:28 +0100

    setting up connection to MongoDB

--- app/app.js
+++ app/app.js
@@ -1,5 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import dbConnect from "../config/dbConnect.js";
+import userRoutes from "../routes/usersRoute.js";
 
+// db Connect
+dbConnect();
 const app = express();
+
+// routes
+app.use("/", userRoutes);
 
 export default app;
