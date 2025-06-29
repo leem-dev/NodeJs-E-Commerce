ed1e8ca66691fdc24900f20f38198e412fd7c04a
Seleem <alabitseleem@gmail.com>
2025-06-29 03:36:18 +0100

    User Login Controller and Error Handling

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
   try {
-    const connected = await mongoose.connect(process.env.MONGO_URl);
+    const connected = await mongoose.connect(process.env.MONGO_URL);
     console.log(`Mongodb connected ${connected.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,19 +1,18 @@
 import User from "../model/User.js";
+import asyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
 
 // @desc      Register user
 // @route     POST /api/v1/users/register
 // @access    Private/Admin
 
-export const registerUserCtrl = async (req, res) => {
+export const registerUserCtrl = asyncHandler(async (req, res) => {
   const { fullname, email, password } = req.body;
   // check if users exist
   const userExists = await User.findOne({ email });
   if (userExists) {
     // throw
-    res.json({
-      msg: "User already exists",
-    });
+    throw new Error("User already exists");
   }
 
   // hash password
@@ -33,4 +32,26 @@ export const registerUserCtrl = async (req, res) => {
     message: "User registered successfully",
     data: user,
   });
-};
+});
+
+// @desc      Login user
+// @route     POST /api/v1/users/login
+// @access    Public
+
+export const loginUserCtrl = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  //  find the user in the db with the email only
+  const userFound = await User.findOne({
+    email,
+  });
+
+  if (userFound && (await bcrypt.compare(password, userFound?.password))) {
+    res.json({
+      status: "success",
+      message: "User logged in successfully",
+      userFound,
+    });
+  } else {
+    throw new Error("Invalid login parameters");
+  }
+});
