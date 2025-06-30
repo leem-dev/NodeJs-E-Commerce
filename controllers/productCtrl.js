b7a0fdf1903b2c8bad7d312d66f0fd1e6da965a4
Seleem <alabitseleem@gmail.com>
2025-06-30 17:15:20 +0100

    filter by product models

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -51,7 +51,59 @@ export const createProductCtrl = asyncHandler(async (req, res) => {
 // @access    Public
 
 export const getProductsCtrl = asyncHandler(async (req, res) => {
-  const products = await Product.find();
+  // query
+  let productQuery = Product.find();
+  const safeQuery = Object.assign({}, req.query);
+  console.log(safeQuery);
+
+  // find user by name
+  if (safeQuery.name) {
+    productQuery = productQuery.find({
+      name: { $regex: safeQuery.name, $options: "i" },
+    });
+  }
+
+  // find user by brand
+  if (safeQuery.brand) {
+    productQuery = productQuery.find({
+      brand: { $regex: safeQuery.brand, $options: "i" },
+    });
+  }
+
+  // find user by category
+  if (safeQuery.category) {
+    productQuery = productQuery.find({
+      category: { $regex: safeQuery.category, $options: "i" },
+    });
+  }
+
+  // find user by colors
+  if (safeQuery.color) {
+    productQuery = productQuery.find({
+      colors: { $regex: safeQuery.colors, $options: "i" },
+    });
+  }
+
+  // find user by colors
+  if (safeQuery.sizes) {
+    productQuery = productQuery.find({
+      sizes: { $regex: safeQuery.sizes, $options: "i" },
+    });
+  }
+
+  // filter by price range
+  if (safeQuery.price) {
+    const priceRange = safeQuery.price.split("-");
+    // gte: greater or equal
+    // lte: less than or equal to
+    productQuery = productQuery.find({
+      price: { $gte: priceRange[0], $lte: priceRange[1] },
+    });
+  }
+
+  // await the query
+  const products = await productQuery;
+
   res.json({
     status: "success",
     products,
